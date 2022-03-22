2c647f12eb761e5430e42b779697483f3a783701
Szymon <100271667+Otlos2002@users.noreply.github.com>
2022-03-22 11:51:54 +0100

    Update arytmetyczny.py

--- praca_domowa_lab3/zad9/arytmetyczny.py
+++ praca_domowa_lab3/zad9/arytmetyczny.py
@@ -1,10 +1,10 @@
 
-def stworz_ciong(a1, r, n):
+def stworz_ciag(a1, r, n):
     lista = [a1 + r * x for x in range(0, n + 1)]
     return lista
 
 
-def suma_elementuw(a1, r, n):
+def suma_elementow(a1, r, n):
     suma = 0
     for x in range(0, n + 1):
         suma += a1 + r * x
