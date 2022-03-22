8a1ed11070616586bfbe03e5d3e502ae9932b0ae
Szymon <100271667+Otlos2002@users.noreply.github.com>
2022-03-22 11:51:11 +0100

    Update geometryczny.py

--- praca_domowa_lab3/zad9/geometryczny.py
+++ praca_domowa_lab3/zad9/geometryczny.py
@@ -1,1 +1,10 @@
+def stworz_ciag(a1, q, n):
+    lista = [a1 * (q ** x) for x in range(0, n + 1)]
+    return lista
 
+
+def suma_elementow(a1, q, n):
+    suma = 0
+    for x in range(0, n + 1):
+        suma += a1 * (q ** x)
+    return
